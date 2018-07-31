ef6e63fd96dd11c0e26d0c19e9b7f933f2a8ef05
csilva <csilva@silvatech.org>
2018-07-31 16:15:15 -0600

    Testing Social Auth Changes.

--- linkyohapp/social_auth_pipeline.py
+++ linkyohapp/social_auth_pipeline.py
@@ -7,17 +7,17 @@ def save_avatar(backend, user, response, *args, **kwargs):
     except Profile.DoesNotExist:
         profile = Profile(user_id=user.id)
     if backend.name == 'facebook':
-        profile.avatar = 'http://graph.facebook.com/%s/picture?type=large' % response['id']
+        profile.avatar = 'https://graph.facebook.com/%s/picture?type=large' % response['id']
 
     profile.save()
 
 
-def save_profile(backend, user, response, *args, **kwargs):
+def save_profile1(backend, user, response, *args, **kwargs):
     if backend.name == 'facebook':
         profile = Profile(user_id=user.id)
         if profile is None:
             profile = Profile(user_id=user.id)
-        profile.avatar = 'http://graph.facebook.com/%s/picture?type=large' % response['id']
+        profile.avatar = 'https://graph.facebook.com/%s/picture?type=large' % response['id']
         profile.email = response.get('email')
         profile.first_name = response.get('first_name')
         profile.last_name = response.get('last_name')
@@ -28,3 +28,22 @@ def save_profile(backend, user, response, *args, **kwargs):
         profile.link = response.get('link')
         profile.timezone = response.get('timezone')
         profile.save()
+
+
+def save_profile(backend, user, response, *args, **kwargs):
+    try:
+        profile = Profile.objects.get(user_id=user.id)
+    except Profile.DoesNotExist:
+        profile = Profile(user_id=user.id)
+    if backend.name == 'facebook':
+        profile.avatar = 'https://graph.facebook.com/%s/picture?type=large' % response['id']
+        profile.email = response.get('email')
+        profile.first_name = response.get('first_name')
+        profile.last_name = response.get('last_name')
+        profile.link = response.get('link')
+        profile.locale = response.get('locale')
+        profile.timezone = response.get('timezone')
+        profile.gender = response.get('gender')
+        profile.link = response.get('link')
+        profile.timezone = response.get('timezone')
+    profile.save()
