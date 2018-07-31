4c79a65594629f6f7c9503798c7370f166fc9df6
csilva <csilva@silvatech.org>
2018-07-31 06:08:42 -0600

    updated urls

--- linkyoh/urls.py
+++ linkyoh/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('linkyohapp.urls')),
-    path('social/', include(('social_django.urls','social'), namespace='social')),
+    path('', include(('social_django.urls','social'), namespace='social')),
     path('auth/', include(('django.contrib.auth.urls','auth'), namespace='auth')),
 
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
