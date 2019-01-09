25ef174fef39a23ca7e9ff9047fbb8ec8f700048
csilva <csilva@silvatech.org>
2019-01-09 16:18:27 -0600

    Widget Tweaks was installed and added to settings apps list. About page was added , Contact page was added with mailing function as well as contact and thank you template. Create gig template and edit_gig template html validation was added. Contact form was adding in form.py. Refactoring was doing in models.py. Respective urls were added for contact, thank you and about. Views contact function was added and updated requirements was added.

--- linkyohapp/forms.py
+++ linkyohapp/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Gig, Location, Review
 
@@ -5,12 +6,32 @@ from .models import Gig, Location, Review
 class GigForm(ModelForm):
     class Meta:
         model = Gig
-        fields = ['title', 'category', 'sub_category', 'description', 'price', 'photo', 'status', 'phone_number',
-                  'address_1','address_2', 'state', 'location']
-        fields_required = ['title', 'category', 'sub_category', 'description', 'price', 'photo', 'status', 'phone_number',
-                  'address_1','state', 'location']
+        fields = ['title',
+                  'category',
+                  'sub_category',
+                  'description',
+                  'price',
+                  'photo',
+                  'status',
+                  'phone_number',
+                  'address_1',
+                  'address_2',
+                  'state',
+                  'location']
+        fields_required = ['title',
+                           'category',
+                           'sub_category',
+                           'description',
+                           'price',
+                           'photo',
+                           'status',
+                           'phone_number',
+                           'address_1',
+                           'state',
+                           'location']
         
     def __init__(self, *args, **kwargs):
+
         super().__init__(*args, **kwargs)
         self.fields['location'].queryset = Location.objects.none()
 
@@ -27,5 +48,17 @@ class GigForm(ModelForm):
 class ReviewForm(ModelForm):
     class Meta:
         model = Review
-        fields = ['content','rating']
-        fields_required = ['content','rating']
+        fields = ['content',
+                  'rating']
+        fields_required = ['content',
+                           'rating']
+
+
+class ContactForm(forms.Form):
+    contact_name = forms.CharField(required=True, label="Name")
+    contact_email = forms.EmailField(required=True, label="Email")
+    content = forms.CharField(
+        required=True,
+        widget=forms.Textarea,
+        label="Message"
+    )
--- linkyohapp/urls.py
+++ linkyohapp/urls.py
@@ -1,31 +1,34 @@
 from django.urls import path
 from django.urls import include
 from django.conf.urls import url
-from .models import State,Location
+from .models import State, Location
 from linkyohapp import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name="home"),
     url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_detail, name='gig_detail'),
     url(r'^my_gigs/$', views.my_gigs, name="my_gigs"),
     url(r'^create_gig/$', views.create_gig, name="create_gig"),
     url(r'^my_gigs/edit/(?P<id>[0-9]+)/$', views.edit_gig, name="edit_gig"),
-    url(r'^profile/(?P<username>\w+)/$', views.profile, name='profile'),
-    path('ajax/load-state/', views.load_states, name='ajax_load_states'),
-    path('ajax/load-state/<int:sid>/', views.load_states, name='ajax_load_states'),
-    path('ajax/load-location/', views.load_locations, name='ajax_load_locations'),
-    path('ajax/load-location/<int:sid>/<int:lid>/', views.load_locations, name='ajax_load_locations'),
-    path('ajax/load-category/', views.load_categories, name='ajax_load_category'),
-    path('ajax/load-category/<int:catid>/', views.load_categories, name='ajax_load_category'),
-    path('ajax/load-sub-category/', views.load_sub_categories, name='ajax_load_sub_category'),
-    path('ajax/load-sub-category/<int:catid>/', views.load_sub_categories, name='ajax_load_sub_category'),
-    path('ajax/load-sub-category/<int:catid>/<int:subcatid>/', views.load_sub_categories, name='ajax_load_sub_category'),
-    path('ajax/load-menu-category/', views.load_menu_categories, name='ajax_load_menu_category'),
+    url(r'^profile/(?P<username>\w+)/$', views.profile, name="profile"),
+    path('ajax/load-state/', views.load_states, name="ajax_load_states"),
+    path('ajax/load-state/<int:sid>/', views.load_states, name="ajax_load_states"),
+    path('ajax/load-location/', views.load_locations, name="ajax_load_locations"),
+    path('ajax/load-location/<int:sid>/<int:lid>/', views.load_locations, name="ajax_load_locations"),
+    path('ajax/load-category/', views.load_categories, name="ajax_load_category"),
+    path('ajax/load-category/<int:catid>/', views.load_categories, name="ajax_load_category"),
+    path('ajax/load-sub-category/', views.load_sub_categories, name="ajax_load_sub_category"),
+    path('ajax/load-sub-category/<int:catid>/', views.load_sub_categories, name="ajax_load_sub_category"),
+    path('ajax/load-sub-category/<int:catid>/<int:subcatid>/', views.load_sub_categories,
+         name="ajax_load_sub_category"),
+    path('ajax/load-menu-category/', views.load_menu_categories, name="ajax_load_menu_category"),
     url(r'^category/(?P<id>[0-9]+)/$', views.category_listings, name="category_listing"),
     url(r'^sub-category/(?P<id>[0-9]+)/$', views.sub_category_listings, name="sub_category_listing"),
-    url(r'^search/$',views.search,name='search'),
+    url(r'^search/$', views.search, name="search"),
     url(r'^terms/$', views.terms, name="terms"),
     url(r'^privacy/$', views.privacy, name="privacy"),
+    url(r'^about-us/$', views.about, name="about"),
+    url(r'contact-us/$', views.contact, name="contact"),
+    url(r'thanks/$', views.thanks, name="thanks")
 
 ]
-
