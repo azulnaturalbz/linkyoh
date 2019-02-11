d99da7e7aa9d0766eb01b77f3db534f7de4e150d
csilva <csilva@silvatech.org>
2019-02-10 19:49:27 -0600

    credentials for local app. Please do not push to git hub

---
+++ credentials.py
@@ -0,0 +1,29 @@
+dbuser = 'csilva'
+dbpassword = 'CSILVA&11894'
+dbname = 'linkyoh'
+dbhome = 'localhost'
+dbport = '5432'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_HOST_USER = 'info@silvatech.org'
+EMAIL_HOST_PASSWORD = 'Password&2017'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = 'info@silvatech.org'
+SEND_TO_EMAIL = 'csilva@silvatech.org'
+
+SOCIAL_AUTH_FACEBOOK_KEY = '256564888275073'
+SOCIAL_AUTH_FACEBOOK_SECRET = '44c7cb87720e0f721eadf3e9867499a6'
+
+SECRET_KEY = 'u&h4&o%yw(og5s14fh6d%22c9re+4gdox)^0^981wd907vipp4'
+
+ALLOWED_HOST = 'linkyoh.silvatech.org'
+ALLOWED_HOST1 = 'linkyoh.com'
+ALLOWED_HOST2 = '0.0.0.0'
+ALLOWED_HOST3 = '127.0.0.1'
+
+merchant_id = "ty996djw5934qcrv"
+public_key = "9btwgv3nxzts5s3j"
+private_key = "a87d8909808d0a92c6fcbd9cf25fc446"
