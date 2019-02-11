2aa416ac85b39aa88d7200b6ea3ef976fece04ae
csilva <csilva@silvatech.org>
2019-02-10 20:04:36 -0600

    credentials for local app and removed content.

--- credentials.py
+++ credentials.py
@@ -1,29 +1,29 @@
-dbuser = 'csilva'
-dbpassword = 'CSILVA&11894'
-dbname = 'linkyoh'
-dbhome = 'localhost'
-dbport = '5432'
+dbuser = ['ADD VALUE HERE']
+dbpassword = ['ADD VALUE HERE']
+dbname = ['ADD VALUE HERE']
+dbhome = ['ADD VALUE HERE']
+dbport = ['ADD VALUE HERE']
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.zoho.com'
-EMAIL_HOST_USER = 'info@silvatech.org'
-EMAIL_HOST_PASSWORD = 'Password&2017'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-DEFAULT_FROM_EMAIL = 'info@silvatech.org'
-SEND_TO_EMAIL = 'csilva@silvatech.org'
+EMAIL_BACKEND = ['ADD VALUE HERE']
+EMAIL_HOST = ['ADD VALUE HERE']
+EMAIL_HOST_USER = ['ADD VALUE HERE']
+EMAIL_HOST_PASSWORD = ['ADD VALUE HERE']
+EMAIL_PORT = [ADD VALUE HERE]
+EMAIL_USE_TLS = [ADD VALUE HERE]
+EMAIL_USE_SSL = [ADD VALUE HERE]
+DEFAULT_FROM_EMAIL = ['ADD VALUE HERE']
+SEND_TO_EMAIL = ['ADD VALUE HERE']
 
-SOCIAL_AUTH_FACEBOOK_KEY = '256564888275073'
-SOCIAL_AUTH_FACEBOOK_SECRET = '44c7cb87720e0f721eadf3e9867499a6'
+SOCIAL_AUTH_FACEBOOK_KEY = ['ADD VALUE HERE']
+SOCIAL_AUTH_FACEBOOK_SECRET = ['ADD VALUE HERE']
 
-SECRET_KEY = 'u&h4&o%yw(og5s14fh6d%22c9re+4gdox)^0^981wd907vipp4'
+SECRET_KEY = ['ADD VALUE HERE']
 
-ALLOWED_HOST = 'linkyoh.silvatech.org'
-ALLOWED_HOST1 = 'linkyoh.com'
-ALLOWED_HOST2 = '0.0.0.0'
-ALLOWED_HOST3 = '127.0.0.1'
+ALLOWED_HOST = ['ADD VALUE HERE']
+ALLOWED_HOST1 = ['ADD VALUE HERE']
+ALLOWED_HOST2 = ['ADD VALUE HERE']
+ALLOWED_HOST3 = ['ADD VALUE HERE']
 
-merchant_id = "ty996djw5934qcrv"
-public_key = "9btwgv3nxzts5s3j"
-private_key = "a87d8909808d0a92c6fcbd9cf25fc446"
+merchant_id = ["ADD VALUE HERE"]
+public_key = ["ADD VALUE HERE"]
+private_key = ["ADD VALUE HERE"]
