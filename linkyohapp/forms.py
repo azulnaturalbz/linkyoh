125f483f5408dd55ae572af45aa04e9cc46c3d59
Cristian Emmanuel Silva <csilva@silvatech.org>
2020-07-03 11:21:04 -0600

    Use client side validation for image in templates and ommited def clean image.

--- linkyohapp/forms.py
+++ linkyohapp/forms.py
@@ -58,14 +58,14 @@ class GigForm(ModelForm):
             self.fields['location'].queryset = self.instance.state.location_set.order_by('local')
 
     # Function Below makes sure that the image is only jpg , jpeg ,png or gif.
-    def clean_photo(self):
-        if self.cleaned_data.get('photo') != "":
-            photo = self.cleaned_data.get('photo')
-            if not photo.name.endswith(".jpg") | photo.name.endswith(".jpeg") | photo.name.endswith(".png") | photo.name.endswith(".gif"):
-                raise forms.ValidationError("Only .jpg image accepted")
-            return photo
-        else:
-            pass
+    # def clean_photo(self):
+    #     if self.cleaned_data.get('photo') != "":
+    #         photo = self.cleaned_data.get('photo')
+    #         if not photo.name.endswith(".jpg") | photo.name.endswith(".jpeg") | photo.name.endswith(".png") | photo.name.endswith(".gif"):
+    #             raise forms.ValidationError("Only .jpg image accepted")
+    #         return photo
+    #     else:
+    #         pass
 
 
 class ReviewForm(ModelForm):
